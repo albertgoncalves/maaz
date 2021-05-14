e69b4d373785985a45a9f86742660b6dc126bc8d
albertgoncalves <goncalves.albert@gmail.com>
2021-05-14 19:41:27 -0400

    change data structure

--- src/main.c
+++ src/main.c
@@ -2,22 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define CAP_LISTS 64
+#define CAP_NODES 64
+#define CAP_EDGES 64
 
 typedef uint8_t u8;
 
 typedef int32_t i32;
 
-typedef struct List List;
-
-struct List {
+typedef struct {
     const char* label;
-    List*       next;
-};
+} Node;
 
 typedef struct {
-    List lists[CAP_LISTS];
-    u8   len_lists;
+    Node* from;
+    Node* to;
+} Edge;
+
+typedef struct {
+    Node nodes[CAP_NODES];
+    u8   len_nodes;
+    Edge edges[CAP_EDGES];
+    u8   len_edges;
 } Memory;
 
 #define EXIT_IF(condition)           \
@@ -31,45 +36,63 @@ typedef struct {
         exit(EXIT_FAILURE);          \
     }
 
-static List* alloc_list(Memory* memory) {
-    EXIT_IF(CAP_LISTS <= memory->len_lists);
-    return &memory->lists[memory->len_lists++];
+static Node* alloc_node(Memory* memory) {
+    EXIT_IF(CAP_NODES <= memory->len_nodes);
+    return &memory->nodes[memory->len_nodes++];
 }
+
+static Edge* alloc_edge(Memory* memory) {
+    EXIT_IF(CAP_EDGES <= memory->len_edges);
+    return &memory->edges[memory->len_edges++];
+}
+
+#define N 5
+
+#define ALLOC_EDGE(memory, i, j)         \
+    {                                    \
+        Edge* edge = alloc_edge(memory); \
+        edge->from = nodes[i];           \
+        edge->to = nodes[j];             \
+    }
 
 i32 main(void) {
     fprintf(stderr,
             "\n"
-            "sizeof(List)   : %zu\n"
+            "sizeof(Node)   : %zu\n"
+            "sizeof(Edge)   : %zu\n"
             "sizeof(Memory) : %zu\n"
             "\n",
-            sizeof(List),
+            sizeof(Node),
+            sizeof(Edge),
             sizeof(Memory));
     Memory* memory = calloc(1, sizeof(Memory));
+    EXIT_IF(!memory);
     {
-        List* a = alloc_list(memory);
-        List* b = alloc_list(memory);
-        List* c = alloc_list(memory);
-        List* d = alloc_list(memory);
-        List* e = alloc_list(memory);
-        a->label = "foo";
-        a->next = b;
-        b->label = "bar ...";
-        b->next = c;
-        c->label = "\tbaz";
-        c->next = a;
-        d->label = "qux";
-        d->next = d;
-        e->label = "???";
-        e->next = NULL;
+        Node* nodes[N];
+        for (u8 i = 0; i < N; ++i) {
+            nodes[i] = alloc_node(memory);
+        }
+        nodes[0]->label = "a";
+        nodes[1]->label = "b";
+        nodes[2]->label = "c";
+        nodes[3]->label = "d";
+        nodes[4]->label = "e";
+        ALLOC_EDGE(memory, 0, 1);
+        ALLOC_EDGE(memory, 0, 2);
+        ALLOC_EDGE(memory, 0, 4);
+        ALLOC_EDGE(memory, 1, 0);
+        ALLOC_EDGE(memory, 1, 2);
+        ALLOC_EDGE(memory, 2, 0);
+        ALLOC_EDGE(memory, 3, 0);
+        ALLOC_EDGE(memory, 4, 2);
+        ALLOC_EDGE(memory, 4, 3);
     }
     {
         printf("digraph {\n");
-        for (u8 i = 0; i < memory->len_lists; ++i) {
-            printf("    \"%s\"", memory->lists[i].label);
-            if (memory->lists[i].next) {
-                printf(" -> \"%s\"", memory->lists[i].next->label);
-            }
-            printf("\n");
+        for (u8 i = 0; i < memory->len_edges; ++i) {
+            Edge edge = memory->edges[i];
+            EXIT_IF((!edge.from) || (!edge.to));
+            printf("    \"%s\" -> \"%s\"", edge.from->label, edge.to->label);
         }
         printf("}\n");
     }
